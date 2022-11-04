0abc5d8c7454ca46084e7137607b86e246d88719
André Rossner <rossnerandre@gmail.com>
2022-11-03 23:20:52 -0300

    add routes

---
+++ src/routes/paths.ts
@@ -0,0 +1,13 @@
+
+
+export const PATH_PAGE = {
+  page403: '/403',
+  page404: '/404',
+  page500: '/500',
+  home: '/',
+  about: '/about',
+  projects: '/projects',
+  contact: '/contact',
+  // politicaDePrivacidade: '/politica-de-privacidade',
+  // politicaDeCookies: '/politica-de-cookies',
+};
