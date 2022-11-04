85d7eea21065a19a4e87b4378c12299516ae52c3
André Rossner <rossnerandre@gmail.com>
2022-11-03 23:19:29 -0300

    Add component Page

---
+++ src/components/Page.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import { forwardRef, ReactNode } from 'react';
+// @mui
+import { Box, BoxProps } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+interface Props extends BoxProps {
+  children: ReactNode;
+  meta?: ReactNode;
+  title: string;
+}
+
+// eslint-disable-next-line react/display-name
+const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, ...other }, ref) => (
+  <>
+    <Head>
+      <title>{`${title} | André Rossner`}</title>
+      {meta}
+    </Head>
+
+    <Box ref={ref} {...other}>
+      {children}
+    </Box>
+  </>
+));
+
+export default Page;
