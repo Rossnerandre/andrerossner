b0531f68b09d489edd0db98cebd94aaf6ec4e261
André Rossner <rossnerandre@gmail.com>
2022-11-03 23:14:28 -0300

    Add components animate with framer motion

---
+++ src/components/animate/MotionContainer.tsx
@@ -0,0 +1,43 @@
+import { motion, MotionProps } from 'framer-motion';
+// @mui
+import { Box, BoxProps } from '@mui/material';
+//
+import { varContainer } from './variants';
+
+// ----------------------------------------------------------------------
+
+type IProps = BoxProps & MotionProps;
+
+export interface Props extends IProps {
+  animate?: boolean;
+  action?: boolean;
+}
+
+export default function MotionContainer({ animate, action = false, children, ...other }: Props) {
+  if (action) {
+    return (
+      <Box
+        component={motion.div}
+        initial={false}
+        animate={animate ? 'animate' : 'exit'}
+        variants={varContainer()}
+        {...other}
+      >
+        {children}
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      component={motion.div}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={varContainer()}
+      {...other}
+    >
+      {children}
+    </Box>
+  );
+}
