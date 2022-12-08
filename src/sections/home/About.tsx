8e03c760496f8bbf12c99acac7fa10f2b1355391
André Rossner <rossnerandre@gmail.com>
2022-12-08 14:18:59 -0300

    Add dados a página About, add stled components para melhorar leitura

--- src/sections/home/About.tsx
+++ src/sections/home/About.tsx
@@ -1,61 +1,80 @@
-import {Box, Container, Grid, Paper, Typography} from "@mui/material";
-import {styled} from "@mui/system";
+import {Container, Grid, Paper, Typography} from "@mui/material";
+import {styled} from '@mui/material/styles';
 import Image from "next/image";
 
-const TesteBox = styled(Box)(() => ({
-  // backgroundColor: theme.palette.background.default,
-  // position: 'relative',
-  // zIndex: '10 !important',
+// ----------------------------------------------------------------------
+
+const RootContainer = styled(Container)(() => ({
+  background: '#333',
+  marginBottom: '5rem',
+  marginTop: '5rem',
 }));
 
-const FormTop = styled(Box)(({theme}) => ({
-  backgroundColor: theme.palette.secondary.dark,
-  position: 'absolute',
-  width: '350px',
-  height: '300px',
-  top: 0,
-  right: 0,
-  clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 31% 68%)',
-  zIndex: '2',
-}));
-
-const ImageStyled = styled(Image)(() => ({
+const ImageStyled = styled(Image)(({theme}) => ({
   borderRadius: '18px',
+  boxShadow: theme.customShadows.z8,
 }));
 
+const GridImg = styled(Grid)(({theme}) => ({
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  [theme.breakpoints.down('md')]: {
+    marginBottom: '3rem',
+  },
+}));
+
+const GridText = styled(Grid)(() => ({
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+}));
+
+const PaperStyled = styled(Paper)(() => ({
+  background: '#333',
+  borderRadius: '18px',
+  padding: '2rem',
+  textAlign: 'justify',
+  color: 'white',
+}));
+
+// ----------------------------------------------------------------------
 
 export default function About() {
   return (
-    <TesteBox>
-      <FormTop/>
-      <Container id={'about'} sx={{background: '#333', zIndex: '12', position: 'relative', marginBottom: '5rem'}}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sx={{mt: 5, mb: 3}} p={0} textAlign={'center'} display={'flex'} justifyContent={'center'}
-                height={'80px'}>
-            <Typography variant={'h2'} color={'secondary'}>About me</Typography>
-          </Grid>
-          <Grid item xs={12} md={6} alignItems={'center'} display={'flex'} flexDirection={'column'}
-                justifyContent={'center'}
-                sx={{mt: {xs: 10, md: 0}}}
-          >
-            <ImageStyled
-              src={'/assets/home/me.jpeg'} alt={'Me André Rossner'} width={300}
-              height={400}/>
-          </Grid>
-          <Grid item xs={12} md={6} alignItems={'center'} display={'flex'} flexDirection={'column'}
-                justifyContent={'center'}
-          >
-            <Box color={'white'} component={Paper} elevation={5} bgcolor={'#333'} p={3}>
-              Hello my name is André Rossner.
-              Graduated in Systems Analysis and Development, working as a FrontEnd Developer, mainly with NEXTJS,
-              TypeScript, API&apos;s, MySQL, among other web development languages.
-              I am very curious about technology, I like sharing ideas and
-              knowledge, I am always interested in learning more. I try to adapt to the needs of the
-              company, and do my best. I am currently working on English to be fluent.
-            </Box>
-          </Grid>
+    <RootContainer id={'about'}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sx={{mb: 5}} textAlign={'center'}>
+          <Typography variant={'h2'} color={'secondary'}>Sobre mim</Typography>
         </Grid>
-      </Container>
-    </TesteBox>
+        <GridImg item xs={12} md={6}>
+          <ImageStyled
+            src={'/assets/home/me.jpeg'} alt={'Me André Rossner'} width={300}
+            height={400}/>
+        </GridImg>
+        <GridText item xs={12} md={6}>
+          <PaperStyled elevation={4}>
+            <Typography variant={'h3'} color={'secondary'} textAlign={'center'}>Olá sou André Rossner</Typography>
+            <Typography variant={'h5'} color={'secondary'} textAlign={'center'}>Desenvolvedor Front-End</Typography>
+            <br/>
+            <Typography variant={'body1'}>
+              Graduarao em Análise e desenvolvimento de sistemas, apaixonado por tecnologia, programação e inovação.
+              Atualmente trabalho com desenvolvimento de software, focado em desenvolvimento web. Tenho experiência
+              com desenvolvimento de aplicações web, utilizando tecnologias e ferramentas como React, TypeScript,
+              JavaScript, HTML, CSS, PHP, MySQL, Firebase, UX/UI, Figma.
+            </Typography>
+            <br/>
+            <Typography variant={'body1'}>
+              Motiviado a aprender novas tecnologias e ferramentas, sempre buscando aperfeiçoamento e crescimento,
+              adoro trocar experiências e conhecimentos com outras pessoas.
+              Busco me adaptar as necessidades do mercado e das empresas, sempre buscando a melhor solução para o
+              cliente.
+            </Typography>
+          </PaperStyled>
+        </GridText>
+      </Grid>
+    </RootContainer>
   )
 }
